c0b0cd8cd496e699ca6a37aeb41ee6ecd10afe3b
juanmanu12-dot <juanmuzou@gmail.com>
2025-08-29 07:14:31 -0500

    Update test.py

--- liquidacion nomina/src/model/test.py
+++ liquidacion nomina/src/model/test.py
@@ -1,4 +1,4 @@
-# Valores fijos
+
 AUX_TRANSPORTE = 162000  # Valor 2025 aprox
 PORC_SALUD = 0.04
 PORC_PENSION = 0.04
@@ -16,21 +16,21 @@ def calcular_valor_hora(salario):
 def calcular_neto_a_pagar(salario, dias, h_extra_d, h_extra_n, h_extra_dom, aplica_aux):
     valor_hora = calcular_valor_hora(salario)
 
-    # Horas extra
+    
     extra_d = h_extra_d * valor_hora * 1.25
     extra_n = h_extra_n * valor_hora * 1.75
     extra_dom = h_extra_dom * valor_hora * 2
 
-    # Salario proporcional a días trabajados
+    
     salario_base = salario * dias / 30
 
-    # Auxilio transporte
+    
     aux = AUX_TRANSPORTE if aplica_aux else 0
 
-    # Ingresos
+
     total_ingresos = salario_base + aux + extra_d + extra_n + extra_dom
 
-    # Deducciones
+    
     salud = salario_base * PORC_SALUD
     pension = salario_base * PORC_PENSION
 
@@ -53,4 +53,5 @@ def calcular_aportes_empleador(salario):
     caja = salario * PORC_CAJA
     icbf = salario * PORC_ICBF
     sena = salario * PORC_SENA
+
     return salud + pension + arl + caja + icbf + sena    
